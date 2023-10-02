b06f36cc1809f8c9fc3652a9d7cc4438cc6f4b0d
Tinkerc <chenruoyun@126.com>
2023-10-02 21:57:19 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,59 +22,52 @@ def createMarkdown(date, filename):
     with open(filename, 'w') as f:
         f.write("## " + date + "\n")
 
+
 def checkPathExist(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+
 def scrape(language, filename):
     HEADERS = {
-        'User-Agent'	: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
-        'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding'	: 'gzip,deflate,sdch',
-        'Accept-Language'	: 'zh-CN,zh;q=0.8'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8'
     }
-
     url = 'https://github.com/trending/{language}'.format(language=language)
     r = requests.get(url, headers=HEADERS)
     assert r.status_code == 200
-
     d = pq(r.content)
     items = d('div.Box article.Box-row')
-
-    # codecs to solve the problem utf-8 codec like chinese
     with codecs.open(filename, "a", "utf-8") as f:
         f.write('\n#### {language}\n'.format(language=language))
-
         for item in items:
             i = pq(item)
             title = i(".lh-condensed a").text()
-            owner = i(".lh-condensed span.text-normal").text()
             description = i("p.col-9").text()
             url = i(".lh-condensed a").attr("href")
             url = "https://github.com" + url
-            # ownerImg = i("p.repo-list-meta a img").attr("src")
-            # print(ownerImg)
             f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
 
 
 def job():
+    # 获取环境变量的值，如果不存在则返回默认值
+    env_variable_value = os.environ.get('username', 'default_value')
+    env_variable_value_password = os.environ.get('password', 'default_value')
+    print(env_variable_value, env_variable_value_password)
+    
     strdate = datetime.datetime.now().strftime('%Y-%m-%d')
     stryear = datetime.datetime.now().strftime('%Y')
-    filename = stryear +'/{date}.md'.format(date=strdate)
-
+    filename = stryear + '/{date}.md'.format(date=strdate)
     checkPathExist(stryear)
-    # create markdown file
     createMarkdown(strdate, filename)
-
-    # write markdown
     scrape('python', filename)
-    scrape('swift', filename)
+    #scrape('swift', filename)
     scrape('javascript', filename)
     scrape('go', filename)
     scrape('java', filename)
 
-    # git add commit push
-    # git_add_commit_push(strdate, filename)
 
 if __name__ == '__main__':
     job()
